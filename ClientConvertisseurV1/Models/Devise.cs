bd91405b5b5f8cf6780e8dd91589b4fca77aea95
AlixLejeune <alix.lejeune@etu.univ-smb.fr>
2024-02-01 11:43:59 +0100

    Ajoutez des fichiers projet.

---
+++ ClientConvertisseurV1/Models/Devise.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientConvertisseurV1.Models
+{
+    public class Devise
+    {
+        private int id;
+        private string? nomDevise;
+        private double taux;
+
+        public int Id { get => id; set { id = value; } }
+        public string? NomDevise { get => nomDevise; set { nomDevise = value; } }
+        public double Taux { get => taux; set { taux = value; } }
+
+        public Devise(int id, string? nomDevise, double taux)
+        {
+            Id = id;
+            NomDevise = nomDevise;
+            Taux = taux;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Devise devise &&
+                   Id == devise.Id &&
+                   NomDevise == devise.NomDevise &&
+                   Taux == devise.Taux;
+        }
+    }
+}
---
+++ ClientConvertisseurV1/Services/WSService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ClientConvertisseurV1.Models;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace ClientConvertisseurV1.Services
+{
+    internal class WSService : IService
+    {
+        private readonly HttpClient client;
+        public WSService(string url)
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            
+        }
+
+        public async Task<List<Devise>> GetDevisesAsync(string nomControleur)
+        {
+            try
+            {
+                return await client.GetFromJsonAsync<List<Devise>>(nomControleur);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
